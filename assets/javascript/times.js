2cec8f1a8543e5f45be9a65562aa5cf09e34ebb1
Daniel Nop <danielnop@lawn-143-215-55-67.lawn.gatech.edu>
2017-11-30 10:58:10 -0500

    corrected links

---
+++ assets/javascript/times.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+	$('#submit').on(function(){
+		e.preventDefault();
+		var searchTerm = $('#sumbitbtn').val();
+		var startyear = $('#styear').val();
+		var endYear = $('#endyear').val();
+
+	})
+	$('#clear').on(fuction(){
+		e.preventDefault();
+
+	})
+})
