d6bebc4e5af94faeaf916f4c3818e4e10f97d530
Michelle Johnson <bell2936@yahoo.com>
2017-12-02 15:15:02 -0500

    frontentclickfunctionbranch

---
+++ assets/javascript/times.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function () {
+
+var search=$("search").val();
+var clearResult=$("clearResult").val();
+
+
+$("search").click(function) {
+	$("#search").val();
+});
+
+&("clearResult").click(function){
+	$("#clearResult").val();
+});
+
+
+
+
